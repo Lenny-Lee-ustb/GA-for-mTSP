5880d5a554b70889eb102a2c7360fc27c7e86b6c
lennylee <1021620522@qq.com>
2021-12-17 15:30:18 +0800

    a version for Zhuolun

---
+++ mtsp_distance/globals.py
@@ -0,0 +1,55 @@
+import random
+import pandas as pd
+import math
+'''
+Contains all global variables specific to simulation
+'''
+# Defines range for coordinates when dustbins are randomly scattered
+xMax = 1000
+yMax = 1000
+seedValue = 1
+# numNodes = 51
+numGenerations = 200
+# size of population
+populationSize = 100
+mutationRate = 0.3
+tournamentSize = 10
+elitism = True
+# number of trucks
+numTrucks = 5
+
+# read differnt dataset
+# city_list =pd.read_csv("pr76.txt",sep=" ",header=0,names=['id','X','Y'])
+# city_list =pd.read_csv("pr152.txt",sep=" ",header=0,names=['id','X','Y'])
+# city_list =pd.read_csv("pr226.txt",sep=" ",header=0,names=['id','X','Y'])
+# city_list =pd.read_csv("mtsp51.txt",sep=" ",header=0,names=['id','X','Y'])
+# city_list =pd.read_csv("mtsp100.txt",sep=" ",header=0,names=['id','X','Y'])
+city_list =pd.read_csv("mtsp150.txt",sep=" ",header=0,names=['id','X','Y'])
+numNodes = len(city_list)
+X = city_list['X'].tolist()
+Y = city_list['Y'].tolist()
+
+dist_map = [[0]*numNodes]*numNodes
+
+
+def random_range(n, total):
+    """Return a randomly chosen list of n positive integers summing to total.
+    Each such list is equally likely to occur."""
+
+    dividers = sorted(random.sample(range(1, total), n - 1))
+    return [a - b for a, b in zip(dividers + [total], [0] + dividers)]
+
+
+# Randomly distribute number of dustbins to subroutes
+# Maximum and minimum values are maintained to reach optimal result
+def route_lengths():
+    upper = (numNodes + numTrucks - 1)
+    fa = upper/numTrucks*1.6 # max route length
+    fb = upper/numTrucks*0.6 # min route length
+    a = random_range(numTrucks, upper)
+    while 1:
+        if all( i < fa and i > fb  for i in a):
+                break
+        else:
+                a = random_range(numTrucks, upper)
+    return a
---
+++ mtsp_distance/main.py
@@ -0,0 +1,40 @@
+from galogic import *
+import matplotlib.pyplot as plt
+import progressbar
+import pandas as pd
+import time
+pbar = progressbar.ProgressBar()
+
+tic = time.time()
+# Add Dustbins
+for i in range(numNodes):
+    RouteManager.addDustbin(Dustbin(X[i],Y[i]))
+
+random.seed(seedValue)
+yaxis = [] # Fittest value (distance)
+xaxis = [] # Generation count
+
+pop = Population(populationSize, True)
+globalRoute = pop.getFittest()
+print ('Initial minimum distance: ' + str(globalRoute.getDistance()))
+
+# Start evolving
+for i in pbar(range(numGenerations)):
+    pop = GA.evolvePopulation(pop)
+    localRoute = pop.getFittest()
+    if globalRoute.getDistance() > localRoute.getDistance():
+        globalRoute = localRoute
+        # print("epochs:",i," distance:",localRoute.getDistance())
+    yaxis.append(localRoute.getDistance())
+    xaxis.append(i)
+
+toc = time.time()
+print("Time cost:",round(toc-tic,2))
+
+print ('Global minimum distance: ' + str(globalRoute.getDistance()))
+print ('Final Route:\n' + globalRoute.toString())
+
+fig = plt.figure()
+
+plt.plot(xaxis, yaxis, 'r-')
+plt.show()
