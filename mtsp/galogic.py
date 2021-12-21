dbdfced5641716c74d9d667793f5378508f55f7d
lennylee <1021620522@qq.com>
2021-12-22 00:15:49 +0800

    repalce the benchmark

--- mtsp/galogic.py
+++ mtsp/galogic.py
@@ -103,8 +103,7 @@ class GA:
         swap1 = [] # values from 1
         swap2 = [] # values from 2
 
-        if random.randrange(100)/100 < mutationRate:
-        # A change! reduce the random rate, former code has 100% mutate 
+        if random.randrange(1) < mutationRate:
             # pop all the values to be replaced
             for i in range(route1startPos, route1lastPos + 1):
                 swap1.append(route.route[index1].pop(route1startPos))
